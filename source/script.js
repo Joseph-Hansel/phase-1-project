6b4ab1ee8f9dbf7c2feebf7d890a6d967932790a
Joseph-Hansel <abogejosephhansel@gmail.com>
2025-06-29 19:28:58 +0300

    update javascript

--- source/script.js
+++ source/script.js
@@ -9,19 +9,28 @@ function run() {
             main.id = `${word}-id`
             main.className = "main"
             display.appendChild(main);
+            let num = 0;
             main.innerHTML = `<h3>${word}</h3>
 
-                <h4>${wording[0].phonetic}</h4>
+                <h4>${wording[num].phonetic}</h4>
 
-                <p>${wording[0].meanings[0].partOfSpeech}</p>
+                <p>${wording[0].meanings[num].partOfSpeech}</p>
 
-                <p>${wording[0].meanings[0].definitions[0].definition}</p>`
+                <p>${wording[0].meanings[num].definitions[0].definition}</p>`
+
             let other = main.appendChild(document.createElement("button"))
             other.innerText = "Other definitions"
             other.className = "actions"
-            let more = main.appendChild(document.createElement("button"))
-            more.innerText = "Know more about this word."
-            more.className = "actions"
+            other.addEventListener("click", ()=> {
+                num ++;
+                main.innerHTML = `<h3>${word}</h3>
+
+                <h4>${wording[0].phonetic}</h4>
+
+                <p>${wording[0].meanings[num].partOfSpeech}</p>
+
+                <p>${wording[0].meanings[num].definitions[0].definition}</p>`
+            })
         });
     })
     
